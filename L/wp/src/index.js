dbf686c02cb8518aa4bedae5f743abbe0a7f4f17
imuntil <imuntil@qq.com>
2019-06-09 18:33:15 +0800

    多入口 entry

--- L/wp/src/index.js
+++ L/wp/src/index.js
@@ -1,8 +1,13 @@
 import _ from 'lodash'
+import printMe from './print'
 
 function component() {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  btn.innerHTML = '点击这里，然后查看console'
+  btn.onclick = printMe
+  element.appendChild(btn)
   return element
 }
 document.body.appendChild(component())
