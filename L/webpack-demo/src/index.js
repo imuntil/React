a1154b5dde9a785a9bd6bd79fd2e318b10425754
imuntil <imuntil@qq.com>
2019-06-29 15:40:16 +0800

    webpack split-chunks

--- L/webpack-demo/src/index.js
+++ L/webpack-demo/src/index.js
@@ -1,15 +1,16 @@
-// import _ from 'lodash'
+import _ from 'lodash'
+import $ from 'jquery'
 
-// console.log(_.join(['a', 'b', 'c'], '***'))
+console.log(_.join(['a', 'b', 'c'], '***'))
 
-function getComponent() {
-  return import('lodash').then(_ => {
-    const element = document.createElement('div')
-    element.innerHTML = _.join(['Hello', 'world'], ' ')
-    return element
-  })
-}
+// function getComponent() {
+//   return import(/* webpackChunkName:"lodash" */ 'lodash').then(_ => {
+//     const element = document.createElement('div')
+//     element.innerHTML = _.join(['Hello', 'world'], ' ')
+//     return element
+//   })
+// }
 
-getComponent().then(element => {
-  document.body.appendChild(element)
-})
+// getComponent().then(element => {
+//   document.body.appendChild(element)
+// })
