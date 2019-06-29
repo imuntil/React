7bb92b7c6fd78d5a3cc476d52b397b56dff815e4
imuntil <imuntil@qq.com>
2019-06-29 17:03:50 +0800

    webpack prefetch

--- L/webpack-demo/src/index.js
+++ L/webpack-demo/src/index.js
@@ -1,19 +1,23 @@
-function getComponent() {
-  return import(/* webpackChunkName:"lodash" */ 'lodash').then(_ => {
-    const element = document.createElement('div')
-    element.innerHTML = _.join(['Hello', 'world'], ' ')
-    return element
-  })
-}
+// function getComponent() {
+//   return import(/* webpackChunkName:"lodash" */ 'lodash').then(_ => {
+//     const element = document.createElement('div')
+//     element.innerHTML = _.join(['Hello', 'world'], ' ')
+//     return element
+//   })
+// }
+
+// getComponent().then(element => {
+//   document.body.appendChild(element)
+// })
+// import(/* webpackChunkName: 'test' */ './test').then(({default: test}) => {
+//   test.run()
+// })
 
 document.addEventListener(
   'click',
   () => {
-    getComponent().then(element => {
-      document.body.appendChild(element)
-    })
-    import(/* webpackChunkName: 'test' */ './test').then(({default: test}) => {
-      test.run()
+    import(/* webpackPrefetch: true */ './test').then(({ default: res }) => {
+      res.run()
     })
   },
   false
