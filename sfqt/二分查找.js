ec7b93e3afc7f58572c7e094654146fe43506604
imuntil <imuntil@qq.com>
2020-06-15 20:13:08 +0800

    refactor: 二分查找

--- sfqt/二分查找.js
+++ sfqt/二分查找.js
@@ -1,4 +1,4 @@
-// 二分查找
+// 二分查找，两边闭合
 function binary_search(arr, target) {
   if (!arr.length) return -1
   let left = 0
@@ -17,7 +17,7 @@ function binary_search(arr, target) {
   return -1
 }
 
-// 左边界
+// 左边界, 两边闭合
 function left_bound(arr, target) {
   if (!arr.length) return -1
   let left = 0
@@ -39,7 +39,7 @@ function left_bound(arr, target) {
   return left
 }
 
-// 右边界
+// 右边界，两边闭合
 function right_bound(arr, target) {
   if (!arr.length) return -1
   let left = 0
@@ -60,4 +60,48 @@ function right_bound(arr, target) {
     return -1
   }
   return right
+}
+
+// 左边界，左闭右开
+function left_bound_2(arr, target) {
+  if (!arr.length) return -1
+  let left = 0
+  let right = arr.length
+  let mid
+  while (left < right) {
+    mid = Math.floor((left + right) / 2)
+    if (arr[mid] === target) {
+      right = mid
+    } else if (arr[mid] > target) {
+      right = mid
+    } else if (arr[mid] < target) {
+      left = mid + 1
+    }
+  }
+  if (arr[left] !== target || left >= arr.length) {
+    return -1
+  }
+  return left
+}
+
+// 右边界，左闭右开
+function right_bound_2(arr, target) {
+  if (!arr.length) return -1
+  let left = 0
+  let right = arr.length
+  let mid
+  while (left < right) {
+    mid = Math.floor((left + right) / 2)
+    if (arr[mid] === target) {
+      left = mid + 1
+    } else if (arr[mid] > target) {
+      right = mid
+    } else if (arr[mid] < target) {
+      left = mid + 1
+    }
+  }
+  if (arr[right] !== target || right < 0) {
+    return -1
+  }
+  return right - 1
 }
