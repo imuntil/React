8823f1d0f10ea00890fe69315c28c29a8f80488a
imuntil <imuntil@qq.com>
2020-11-18 20:50:52 +0800

    feat: 99

--- leecode/99. 恢复二叉搜索树.js
+++ leecode/99. 恢复二叉搜索树.js
@@ -10,4 +10,45 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
-var recoverTree = function (root) {}
+var recoverTree = function (root) {
+  if (!root) return null
+  const rootCopy = root
+  const queue = []
+  let preNode = new TreeNode(Number.MIN_SAFE_INTEGER)
+  let t = []
+
+  while (root || queue.length) {
+    while (root) {
+      queue.push(root)
+      root = root.left
+    }
+
+    root = queue.pop()
+
+    if (root.val <= preNode.val) {
+      t = [...t, preNode, root]
+      if (t.length === 4) break
+      if (t[2] && t[2] === t[1]) break
+    }
+
+    preNode = root
+    root = root.right
+  }
+
+  // xxxxx wrong!!!!!!!
+  // const x = t[0]
+  // const y = t[t.length - 1]
+  // const { left, right } = x
+  // x.left = y.left
+  // x.right = y.right
+  // y.left = left
+  // y.right = right
+
+  return rootCopy.val === x.val ? y : rootCopy
+}
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
