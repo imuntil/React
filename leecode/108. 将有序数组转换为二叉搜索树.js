f9b04a2e9804b2054b8da9cde9dd90e31d6bceaf
imuntil <imuntil@qq.com>
2020-11-25 20:33:24 +0800

    feat: 109

--- leecode/108. 将有序数组转换为二叉搜索树.js
+++ leecode/108. 将有序数组转换为二叉搜索树.js
@@ -9,6 +9,22 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var sortedArrayToBST = function(nums) {
+var sortedArrayToBST = function (nums) {
+  if (!nums || !nums.length) return null
+  return tr(nums, 0, nums.length - 1)
+}
 
-};
+function tr(nums, start, end) {
+  if (start > end) return null
+  if (start === end) return new TreeNode(nums[start])
+  const mid = Math.floor((end - start) / 2) + start
+  const root = new TreeNode(nums[mid])
+  root.left = tr(nums, start, mid - 1)
+  root.right = tr(nums, mid + 1, end)
+  return root
+}
+
+function TreeNode(val) {
+  this.val = val
+  this.left = this.right = null
+}
---
+++ leecode/109. 有序链表转换二叉搜索树.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function (head) {
+  if (!head) return null
+  const nums = []
+  while (head) {
+    nums.push(head.val)
+    head = head.next
+  }
+
+  const helper = (start, end) => {
+    if (start === end) return new TreeNode(nums[start])
+    const mid = (start + end) >> 1
+    const root = new TreeNode(nums[mid])
+    root.left = helper(start, mid - 1)
+    root.right = helper(mid + 1, end)
+    return root
+  }
+
+  return helper(0, nums.length - 1)
+}
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
